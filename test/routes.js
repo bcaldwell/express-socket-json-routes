b5fc8d5433c0b51f2fe1e3b03c27733894d38708
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2015-08-16 20:50:53 -0400

    Update routes.js

--- test/routes.js
+++ test/routes.js
@@ -2,3 +2,8 @@ var test = require('unit.js');
 var example = 'hello';
 console.log (example);
 test.string(example);
+test.string('hello');
+test.object(user).hasProperty('email');
+test.array(fruit).hasValue('kiwi');
+test.assert(myVar === true);
+test.bool(myVar).isTrue();
